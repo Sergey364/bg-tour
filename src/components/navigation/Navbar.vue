d1d471c0dd8c4919f62504162daf6aa6030304e7
Sergey364 <sergey.mixailov@gmail.com>
2020-10-14 22:50:37 +0300

    add navbar component

---
+++ src/components/navigation/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+<a href="{{item.link}}" class="navigation-navbar__item" title="{{item.title}}">{{item.title}}</a>
+</template>
+
+<style scoped>
+.navigation-navbar__item {
+    display: flex;
+    border-radius: 50%;
+}
+
+.navigation-navbar__item:hover {
+    background-color: rgb(89, 124, 155)
+}
+</style>
---
+++ src/components/navigation/NavigationItems.ts
@@ -0,0 +1,16 @@
+import INavigationItem  from './interfaces/INavigationItem';
+
+const ITEMS: INavigationItem[] = [{
+    title: 'лол',
+    link: '#'
+},
+{
+    title: 'kek',
+    link: '#'
+}, 
+{
+    title: 'cheburek',
+    link: '#'
+}]
+
+export default ITEMS;
